173ac7e209fe2ea254aff9d0dabd88f6048f4ecb
Yashas PS <53535790+Yashas-pissay@users.noreply.github.com>
2023-03-14 20:42:41 +0530

    Logger file Created

--- demo.py
+++ demo.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 @app.route('/', methods=['GET', 'POST'])
 def index():
     logging.info("We are just testing loggin module")
-    return "Hello World"
+    return "Hello World !!"
 
 if __name__=="__main__":
     app.run(debug = True)
